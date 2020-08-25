f3e1eada01c0ee3a5631b70963a9cf725861f1dd
liyang <15963383124@163.com>
2020-08-25 10:16:12 +0800

    fix bug

--- middleware/db.js
+++ middleware/db.js
@@ -1,12 +1,31 @@
 //加载mysql模块
-var mysql      = require('mysql');
-//创建连接
-var connection = mysql.createConnection({
-host     : '118.31.6.0',
-user     : 'root',
-password : 'Ruan410093793.',
-database : 'account'
-});
-//执行创建连接 
-connection.connect();
-module.exports = connection;
+var mysql = require('mysql');
+
+var config = {
+    host     : '118.31.6.0',
+    user     : 'root',
+    password : 'Ruan410093793.',
+    database : 'account'
+}
+
+function handleError (err) {
+    if (err) {
+      // 如果是连接断开，自动重新连接
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        connect();
+      } else {
+        console.error(err.stack || err);
+      }
+    }
+  }
+  
+  // 连接数据库
+  function connect () {
+    db = mysql.createConnection(config);
+    db.connect(handleError);
+    db.on('error', handleError);
+  }
+  
+  var db;
+  connect();
+  module.exports = db;
